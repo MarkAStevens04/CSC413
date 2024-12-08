45802f3bb7b005463a7322061927e38a4abe8a0b
Mark <doylethecat@gmail.com>
2024-12-08 00:01:34 -0500

    [CODE] cleanup
    Did some light cleanup in the sequence parsing. Added better descriptions to each step.

--- parse_seq.py
+++ parse_seq.py
@@ -78,57 +78,70 @@ class Sequence_Parser():
         Processes a list of amino acids!
         Iterates through each amino acid, creates two lists: given and target.
 
-        :param amino_list: [amino index, 1-letter abbrev, (atom 1), (atom 2), ...] where (atom1) = (atom code, [x, y, z])
-        :return:
+        = Potential error! =
+       -  Assumes amino_list is SORTED! Should be sorted in standard format.
+       -  Assumes provided aminos never have more atoms than the standard aminos do.
+
+        = For future =
+        Possibly remove hydrogen positions, as it is very infrequent for these to be actually included in an mmCif file.
+
+        :param amino_list: List of aminos, each containing list of atoms with some meta-data. Has format: [[amino position, 1-letter abbrev, (atom 1), (atom 2), ...], ...] where (atom1) = (atom code, [x, y, z])
+        :return: given and target list.
+        Given has format [[atom name index, u, v, w, amino name index], ...].
+        u, v, and w are "expert" positions. That is to say, they are the typical positions of each atom in the amino acid.
+        Target has format [atom name index, x, y, z, known_position flag].
+        x, y, and z are the known to be true positions of each atom. Our known_position flag is a 0 if the position is unknown, and x = y = z = -1, and x, y, and z take on the correct values if the flag is 1 (meaning the position is known).
+
         """
-        # print(f'processing {amino}')
 
         # aa_idx will track the current index of our amino acid
         aa_idx = 0
+        # processed_given and processed_target will store a list of our atoms
         processed_given = []
         processed_target = []
         # for every letter in our sequence
         for idx, a, s in zip(range(len(self.ref_seq)), self.pos_seq, self.ref_seq):
-            # print()
-            # print()
-            # print()
-            # print(f'new letter!!!')
-            # just iterate through known amino atom positions...
+
+            # Check whether this amino acid has a known position or not.
             if a != '-':
-                # set our current amino to the current one from our amino list
+                # We know that this amino has a known position!
+
+                # Set our current amino to the current one from our amino list
                 amino = amino_list[aa_idx]
                 aa_idx += 1
-                # will slide through every atom.
+                # will slide through every atom. i will track the provided amino, j will track the standard amino.
                 i = 0
                 j = 0
-                end = False
                 # Want to search through every possible atom in the amino acid.
                 # Only possible for current amino to be shorter. In this case, we
                 # substitute unknown atoms with that flag.
+
+                # while we have not searched through every atom in the standard amino acid...
                 while j < len(self.e.aminos[a]):
-                    # our given atom is the atom of the expert, alongside the name of the residue.
+
                     aa_atom = self.e.aminos[a][j]
                     # Second line converts our letter for amino acid into an index
                     # atom_given = [*aa_atom, a]
                     atom_given = [*aa_atom, ord(a.lower()) - 97]
                     if i + 2 >= len(amino):
-                        # We know we have searched through every known atom!
-                        # Append the atom to the lists still, but with undefined positions.
-                        # print(f'not included :(')
-                        # target atom has unknown position
+                        # We know we have searched through every atom in the given amino!
+                        # Still append the atom from standard amino to the target, but with undefined positions.
                         atom_target = [aa_atom[0], -1, -1, -1, 0]
                         processed_given.append(atom_given)
                         processed_target.append(atom_target)
                         j += 1
                     else:
-                        # We will search through the current atom
+                        # Check the current atom in our given amino. (i + 2 because first 2 indices store amino position and name, respectively)
+                        # Curr atom has format (atom name string, [x, y, z])
                         curr_atom = amino[i + 2]
                         curr_atom_label = self.e.encode[curr_atom[0]]
 
-                        # Check if the current atom is the same as the supposed amino atom
+                        # Check if the given atom is the same as the standard amino atom
                         if int(aa_atom[0]) == curr_atom_label:
-                            # print(f'slay we have a match')
-                            # our target has a known position equal to its given position
+                            # Our current atom is the same as the standard amino atom!
+                            # We slide our frame for both our given amino and standard amino.
+
+                            # Our target has a known position
                             atom_target = [aa_atom[0], *curr_atom[1], 1]
                             processed_given.append(atom_given)
                             processed_target.append(atom_target)
@@ -137,64 +150,56 @@ class Sequence_Parser():
                             j += 1
 
                         elif curr_atom[0] not in self.e.encode:
-                            # if the current atom does not exist in our encoding...
-                            # we know it is a nonstandard atom, and we do NOT add it to our list!
-                            logger.warning(f'nonstandard atom! {curr_atom[0]}')
+                            # If the name of the current atom has never been seen in our standard aminos...
+                            # We know it is likely a nonstandard atom, and we will NOT add it to our list!
+
+                            logger.warning(f'nonstandard atom! {curr_atom[0]}, amino: {amino}')
                             print(f'nonstandard atom...')
-                            # Do not add to target amino!
+                            # Do not add to target!
                             print(f'atom {curr_atom[0]}')
                             i += 1
                         else:
-                            # if there's no match, we will continue scanning in the amino acid.
-                            # Mark that the position is undefined.
-                            # print(f'no match :( {aa_atom[0]}, {curr_atom_label}')
-                            # target atom has unknown position
+                            # We know that the current atom in given and current atom in the standard are not a match.
+                            # We assume that given amino's atoms are a subset of the standard amino's atoms.
+                            # So we know that this standard atom is not in the given amino.
+                            # We add the atom to our target list with an undefined position.
+                            # If there's no match, we will continue scanning in the amino acid.
+
                             atom_target = [aa_atom[0], -1, -1, -1, 0]
                             processed_given.append(atom_given)
                             processed_target.append(atom_target)
                             j += 1
 
-                # Check that we actually inserted all atoms from our amino acid
+                # Error checking. Make sure every atom from our amino has been inserted.
+                # This error will occur if there is an atom out-of-order, or if for some reason
+                # the given amino's atoms are not a subset of the standard amino's atoms.
                 if i != len(amino) - 2:
                     print(f'BOUNDS ERROR!!!! {len(amino)}, {i}')
                     logger.warning(f'BOUNDS ERROR!!! Possibly invalid protein! Length of given amino: {len(amino) - 2}, got to {i}, amino: {amino}')
 
             elif s != '-':
-                # We know the atom does NOT have any defined position!
-                # But it is a valid atom in our sequence
+                # We know the amino does NOT have any defined position!
+                # But it is a valid amino in our sequence
                 j = 0
                 while j < len(self.e.aminos[s]):
-                    # our given atom is the atom of the expert, alongside the name of the residue.
+                    # We will iterate through each atom in our standard amino.
+                    # Add each atom to both given and target lists, but with
+                    # no defined position in our target.
 
                     aa_atom = self.e.aminos[s][j]
 
-                    # We know we have searched through every known atom!
-                    # Append the atom to the lists still, but with undefined positions.
-
-                    # target atom has unknown position
-                    # still regular given atom
-                    atom_target = [aa_atom[0], -1, -1, -1, 0]
-                    # atom_given = [*aa_atom, s]
                     atom_given = [*aa_atom, ord(s.lower()) - 97]
+                    atom_target = [aa_atom[0], -1, -1, -1, 0]
 
                     processed_given.append(atom_given)
                     processed_target.append(atom_target)
                     j += 1
-                # Still want to produce everything we have!
-                # print(f'not processing this yet...')
 
-            # processed_given.append(atom_given)
-            # processed_target.append(atom_target)
-
-        # print(f'finished!')
-        # print()
-        # print()
-        # print()
-        # print(f'input: ')
+        # Turn our atom lists into numpy arrays.
+        # This will allow us to store the binaries of the numpy arrays and access them via
+        # memory mapping.
         input = np.array(processed_given, dtype='f')
         output = np.array(processed_target, dtype='f')
-        # print(input)
-        # print(input.shape)
 
         return input, output
 
@@ -209,6 +214,7 @@ class Sequence_Parser():
         given = np.load(f'{self.bin_dir}{name}-in.npy', mmap_mode='r', allow_pickle=True)
         print(f'train: ')
         print(given)
+        print()
 
         target = np.load(f'{self.bin_dir}{name}-target.npy', mmap_mode='r', allow_pickle=True)
         print(f'target: ')
@@ -254,5 +260,5 @@ if __name__ == '__main__':
 
     print(f'parsing')
     a = Sequence_Parser()
-    # a.get_structs()
-    a.open_struct('6L6Y')
+    a.get_structs()
+    # a.open_struct('6L6Y')
